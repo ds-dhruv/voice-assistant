c82dbe9eaa94c1c5f35cfbb9aa0f294355d5bf4b
Dhruv Sharma <132000465+ds-dhruv@users.noreply.github.com>
2024-07-16 03:23:35 -0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+ 
+# pip install openai 
+
+client = OpenAI(
+  api_key="",
+)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a virtual assistant named jarvis skilled in general tasks like Alexa and Google Cloud"},
+    {"role": "user", "content": "what is coding"}
+  ]
+)
+
+print(completion.choices[0].message.content)
+
+
---
+++ musicLibrary.py
@@ -0,0 +1,14 @@
+music = {
+    "shayad": "https://www.youtube.com/watch?v=MJyKN-8UncM&pp=ygUGc2hheWFk",
+    "see": "https://www.youtube.com/watch?v=RgKAFK5djSk&pp=ygUNc2VlIHlvdSBhZ2Fpbg%3D%3D",
+    "hurts": "https://www.youtube.com/watch?v=4fqwVBuunxY&pp=ygUNaHVydHMgc28gZ29vZA%3D%3D",
+    "strangers": "https://www.youtube.com/watch?v=EIadKxo1ZBs&pp=ygUgc3RyYW5nZXJzIGJ5IGtlbnlhIGdyYWNlIHNwZWQgdXA%3D",
+    "admiring" : "https://www.youtube.com/watch?v=k85UB5b6pJU&pp=ygUMYWRtaXJpbmcgeW91",
+    "nine" : "https://www.youtube.com/watch?v=bzSn6AKLkMI&pp=ygUEOSA0NQ%3D%3D",
+    "attention" : "https://www.youtube.com/watch?v=nfs8NYg7yQM&pp=ygUJYXR0ZW50aW9u",
+    "hass" : "https://www.youtube.com/watch?v=jADTdg-o8i0&pp=ygUYaGFzcyBoYXNzIGRpbGppdCBkb3Nhbmpo",
+    "talk" : "https://www.youtube.com/watch?v=3AtDnEC4zak&pp=ygUVd2UgZG9uJ3QgdGFsayBhbnltb3Jl",
+    "starboy" : "https://www.youtube.com/watch?v=34Na4j8AVgA&pp=ygUHc3RhcmJveQ%3D%3D",
+    "jawan" : "https://www.youtube.com/watch?v=EiScA8DlzLA&pp=ygUFamF3YW4%3D"
+}
+
